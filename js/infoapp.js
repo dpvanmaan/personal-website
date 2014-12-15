1cf51bc164ff28286b607e7b7fa318f1f8793bec
David vanMaanen <david@vanmaanen.us>
2014-12-15 15:25:23 -0500

    Basic info app is added and working

---
+++ js/infoapp.js
@@ -0,0 +1,32 @@
+(function () {
+
+    var app= angular.module("infodisplay",[]);
+    app.run(['$rootScope','$http',function($rootscope,$http){
+	$rootscope.info_dat=[];
+	$http({
+	    url: '/res/resume_info.json',
+	    method: 'GET',
+	}).success(function(data){
+	    $rootscope.info_dat=data.InfoArray;
+	    for (i=0; i<$rootscope.info_dat; i++){
+		$rootscope.info_dat[i].hovered=false;
+	    }
+	    $rootscope.select_infoInd=0;
+	    $rootscope.$broadcast('Ready','');
+		
+	});	
+    }]);
+    app.controller("infolist",['$scope', function($scope){
+	$scope.select_infoInd=0;
+	$scope.InfoClick=function(index){
+	    $scope.select_infoInd=index;
+	};
+	$scope.hover_circl_on=function(index){
+	    $scope.info_dat[index].hovered=true;
+	};
+	$scope.hover_circl_off=function(index){
+	    $scope.info_dat[index].hovered=false;
+	};
+    }])
+
+})();
